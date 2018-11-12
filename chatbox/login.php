fcf9e12b7baf7105ce8081ba935b6c8f1e18ab36
Sadeek A Been <sadiikbeen@gmail.com>
2018-11-11 22:24:32 -0500

    added the files, still in development

---
+++ chatbox/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>chat box system</title>
+</head>
+<body>
+	<div class="login-box">
+		<h1>Login Here</h1>
+		<form action="" method="POST">
+		  <input type="text" name="email" placeholder="Enter Email">
+		  <input type="password" name="password" placeholder="Enter password">
+		  <input type="submit" name="swubmit" value="Login">
+		  <a href="registration.php">Register</a>
+
+	</div>
+
+</body>
+</html>
---
+++ chatbox/registration.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>chat box system</title>
+</head>
+<body>
+	<div class="login-box">
+	<h1>Register Here</h1>
+	<form action="" method="POST" enctype="multipart/form-data">
+	<inut type="text" name ="name" placeholder="Enter username">
+	<input type="email" name="email" placeholder="Enter Email">
+	<input type="password" name= "password" placeholder="Enter Password">
+	<input type="file" name="photo">
+	<input type="submit" name="submit" value="Join">
+	<a href="login.php">Already registered</a>
+	</form>
+</div>	
+
+</body>
+</html>
